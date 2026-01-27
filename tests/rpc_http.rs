e40c8fe2ebd18d0ead8d89c52b417f22743f406f
ctommy <nordik.tommy@gmail.com>
2026-01-26 19:06:17 -0500

    test: cover rpc events queue drain

--- tests/rpc_http.rs
+++ tests/rpc_http.rs
@@ -1,4 +1,7 @@
-use reticulum::rpc::{codec::{decode_frame, encode_frame}, RpcDaemon, RpcRequest, RpcResponse};
+use reticulum::rpc::{
+    codec::{decode_frame, encode_frame},
+    RpcDaemon, RpcEvent, RpcRequest, RpcResponse,
+};
 use reticulum::storage::messages::MessagesStore;
 
 #[test]
@@ -29,4 +32,42 @@ fn rpc_http_roundtrip() {
         + 4;
     let resp: RpcResponse = decode_frame(&response[body_start..]).unwrap();
     assert_eq!(resp.id, 1);
+}
+
+#[test]
+fn rpc_http_events_returns_inbound() {
+    let store = MessagesStore::in_memory().unwrap();
+    let daemon = RpcDaemon::with_store(store, "daemon".into());
+    daemon.inject_inbound_test_message("hello");
+
+    let request_bytes = b"GET /events HTTP/1.1\r\nHost: localhost\r\n\r\n".to_vec();
+    let response = reticulum::rpc::http::handle_http_request(&daemon, &request_bytes).unwrap();
+    let body_start = response
+        .windows(4)
+        .position(|window| window == b"\r\n\r\n")
+        .unwrap()
+        + 4;
+    let event: RpcEvent = decode_frame(&response[body_start..]).unwrap();
+    assert_eq!(event.event_type, "inbound");
+}
+
+#[test]
+fn rpc_http_events_drains_queue() {
+    let store = MessagesStore::in_memory().unwrap();
+    let daemon = RpcDaemon::with_store(store, "daemon".into());
+    daemon.push_event(RpcEvent {
+        event_type: "one".into(),
+        payload: serde_json::json!({ "i": 1 }),
+    });
+
+    let request_bytes = b"GET /events HTTP/1.1\r\nHost: localhost\r\n\r\n".to_vec();
+    let response = reticulum::rpc::http::handle_http_request(&daemon, &request_bytes).unwrap();
+    let body_start = response
+        .windows(4)
+        .position(|window| window == b"\r\n\r\n")
+        .unwrap()
+        + 4;
+    let event: RpcEvent = decode_frame(&response[body_start..]).unwrap();
+    assert_eq!(event.event_type, "one");
+    assert!(daemon.take_event().is_none());
 }
